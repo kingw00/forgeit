59685fe1edf4a28c259de97caa02a93adddbd9bb
kingw00 <zxcat174@gmail.com>
2026-05-27 14:50:09 +0500

    Updated build.sh;

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-mkdir -p build && cd build 
-cmake ..
-cmake --build .
+set -e
+
+echo "[ForgeIt Build]: Initialize build."
+
+mkdir -p build 
+cmake -S . -B build
+
+cmake --build build --parallel ${nproc}
+
+echo "[ForgeIt Build]: the build was successful. The binary file is located in ./build/."
